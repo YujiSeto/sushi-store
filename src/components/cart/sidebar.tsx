1b55510af51c36ad26ba2ccf592e7d7fb2b71cd4
Rodrigo Yuji Seto Soma <rodrigo.y.seto@gmail.com>
2026-02-10 21:47:18 +0900

    feat: enhance cart sidebar with subtotal calculation and item display; update product names and prices for consistency; add CartItem and CartItemQuantity components

--- src/components/cart/sidebar.tsx
+++ src/components/cart/sidebar.tsx
@@ -1,15 +1,28 @@
+"use client";
+
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import { ShoppingCart } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
+import { useCartStore } from "@/stores/cart-store";
+import { Cart } from "@/types/cart";
+import { CartItem } from "./item";
 
 export const CartSideBar = () => {
+  const { cart } = useCartStore((state) => state);
+
+  let subtotal = 0;
+  for (const item of cart) {
+    subtotal += item.quantity * item.product.price;
+  }
+
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button>
+        <Button className="relative">
           <ShoppingCart className="mr-2" />
           <p>Cart</p>
+          {cart.length > 0 && <div className="absolute size-3 bg-red-600 rounded-full -right-1 -top-1" />}
         </Button>
       </SheetTrigger>
       <SheetContent className="p-4">
@@ -20,19 +33,23 @@ export const CartSideBar = () => {
           </div>
         </SheetHeader>
 
-        <div className="flex flex-col gap-5 my-3">...</div>
+        <div className="flex flex-col gap-5 my-3">
+          {cart.map((item: Cart) => (
+            <CartItem key={item.product.id} item={item} />
+          ))}
+        </div>
 
         <Separator className="my-4" />
 
-        <div className="flex justify-between items-center text-xs">
+        <div className="flex justify-between items-center text-md">
           <div>Subtotal</div>
-          <div>$0.00</div>
+          <div>$ {subtotal.toFixed(2)}</div>
         </div>
 
         <Separator className="my-4" />
 
         <div className="text-center">
-          <Button>Complete Purchase</Button>
+          <Button disabled={cart.length === 0}>Complete Purchase</Button>
         </div>
       </SheetContent>
     </Sheet>
