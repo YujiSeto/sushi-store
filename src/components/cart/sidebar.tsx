cc32e5fc614a73b9571efbe5ed7d7bfa2bc9eadd
Rodrigo Yuji Seto Soma <rodrigo.y.seto@gmail.com>
2026-02-10 15:35:42 +0900

    fix: replace RocketIcon with ShoppingCart in CartSideBar and adjust layout

--- src/components/cart/sidebar.tsx
+++ src/components/cart/sidebar.tsx
@@ -1,6 +1,6 @@
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
-import { RocketIcon } from "lucide-react";
+import { ShoppingCart } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 
 export const CartSideBar = () => {
@@ -8,13 +8,16 @@ export const CartSideBar = () => {
     <Sheet>
       <SheetTrigger asChild>
         <Button>
-          <RocketIcon className="mr-2" />
+          <ShoppingCart className="mr-2" />
           <p>Cart</p>
         </Button>
       </SheetTrigger>
       <SheetContent className="p-4">
         <SheetHeader>
-          <SheetTitle>Cart</SheetTitle>
+          <div className="flex items-center gap-2">
+            <ShoppingCart />
+            <SheetTitle>Cart</SheetTitle>
+          </div>
         </SheetHeader>
 
         <div className="flex flex-col gap-5 my-3">...</div>
